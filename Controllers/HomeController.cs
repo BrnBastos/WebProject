52cdf1747aa24a1e5ae563896507d688728d95b8
BrnBastos <bruninhosbastos0228@gmail.com>
2021-06-18 18:53:21 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebProject.Models;
+using System.Web.Mvc;
+
+namespace WebProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var projeto = new Projeto();
+            return View(projeto);
+        }
+        [HttpPost]
+
+        public ActionResult Index(Projeto projeto)
+        {
+            if(ModelState.IsValid)
+            {   
+                return View("Resultado",projeto);
+            }
+            return View(projeto);
+        }
+        public ActionResult Resultado(Projeto projeto)
+        {
+            return View(projeto);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
---
+++ Models/Projeto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebProject.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebProject.Models
+{
+    public class Projeto
+    {
+        [Range(0, 3000, ErrorMessage = "o valor deve ser positivo")]
+        public int Codigo { get; set; }
+        
+        [Required(ErrorMessage = "este campo é obrigatório")]
+        [StringLength(15, MinimumLength = 1, ErrorMessage = "número de caracteres inválido max30")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "este campo é obrigatório")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "número de caracteres inválido")]
+        public string Descricao { get; set; }
+        [Range(0, 30000, ErrorMessage = "o peso válido é entre 0 e 30000kg")] 
+        public int Peso { get; set; }
+        [Required(ErrorMessage = "este campo é obrigatório")]
+        [StringLength(30, MinimumLength = 1, ErrorMessage = "número de caracteres inválido max30")]
+        public string Categoria { get; set; }
+        [Range(0, 300, ErrorMessage = "o valor deve ser entre 1 e 100")] 
+        public double ValorUnitario { get; set; }
+   
+
+
+    }
+}
